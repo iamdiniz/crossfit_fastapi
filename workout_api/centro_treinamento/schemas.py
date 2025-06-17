e5524815e7338545229cc6a78f432fd8c18e7bb5
iamdiniz <guilhermeplays03@gmail.com>
2025-06-17 11:30:29 -0300

    criando rotas para centro de treinamento

--- workout_api/centro_treinamento/schemas.py
+++ workout_api/centro_treinamento/schemas.py
@@ -1,10 +1,18 @@
 
 from typing import Annotated
-from pydantic import Field
+from pydantic import UUID4, Field
 from workout_api.contrib.schemas import BaseSchema
 
 
-class CentroTreinamento(BaseSchema):
+class CentroTreinamentoIn(BaseSchema):
     nome: Annotated[str, Field(description="Nome do centro de treinamento", example='Alliance', max_length=20)]
     endereco: Annotated[str, Field(description="Endereço do centro de treinamento", example='Rua das Flores, 123', max_length=60)]
     proprietario: Annotated[str, Field(description="Nome do proprietário do centro de treinamento", example='João Silva', max_length=30)]
+    
+
+class CentroTreinamentoAtleta(CentroTreinamentoIn):
+    nome: Annotated[str, Field(description="Nome do centro de treinamento", example='Alliance', max_length=20)]
+
+
+class CentroTreinamentoOut(CentroTreinamentoIn):
+    id: Annotated[UUID4, Field(description='Identificador do centro de treinamento')]
