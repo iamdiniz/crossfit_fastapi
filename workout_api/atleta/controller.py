435c245ff9a46a31cadd38de4e1e47eac350dabb
iamdiniz <guilhermeplays03@gmail.com>
2025-06-17 10:07:46 -0300

    mais passos do nosso post

--- workout_api/atleta/controller.py
+++ workout_api/atleta/controller.py
@@ -1,7 +1,15 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Body, status
+
+from workout_api.atleta.schemas import AtletaIn
+from workout_api.contrib.dependencies import DatabaseDependency
 
 router = APIRouter()
 
-@router.post('/')
-async def post():
+@router.post(
+    '/',
+    summary='Criar novo atleta',
+    status_code=status.HTTP_201_CREATED
+)
+async def post(db_session: DatabaseDependency,
+               atleta_in: AtletaIn = Body(...)):
     pass
--- workout_api/configs/database.py
+++ workout_api/configs/database.py
@@ -9,6 +9,6 @@ async_session = sessionmaker(
     engine, class_=AsyncSession, expire_on_commit=False
 )
 
-async def get_sessiont() -> AsyncGenerator:
+async def get_session() -> AsyncGenerator:
     async with async_session() as session:
         yield session
